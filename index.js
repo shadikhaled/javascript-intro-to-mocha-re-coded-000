f47f980b34d5e5aa999142b87c54b4fcdcda235c
Shadi Khaled <shadikhaled777@gmail.com>
2017-03-14 00:16:10 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function theTruth(){
+	return true;
+}
+function sayHey(){
+	return 'Hey!'
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+function shouting(s){
+	return s.toUpperCase();
+
+}
+function roundDown(num){
+return Math.floor(num);
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
